7ac8b7f71469fdc144c00b8f4b97ffa2904c8d4c
Damon Oehlman <damon.oehlman@gmail.com>
2014-03-28 16:03:19 +1100

    Initial conversion complete

--- definition.js
+++ definition.js
@@ -1,93 +1,94 @@
 /* jshint node: true */
 'use strict';
 
-var defaults = require('./defaults');
-var registry = require('./registry');
 var _ = require('underscore');
 
-/**
-  ## TaskDefinition
-**/
-function TaskDefinition(name, opts) {
-  if (! (this instanceof TaskDefinition)) {
-    return new TaskDefinition(name, opts);
+module.exports = function(defaults) {
+  var deps = (defaults || {}).deps || [];
+
+  /**
+    ## TaskDefinition
+  **/
+  function TaskDefinition(name, opts) {
+    if (! (this instanceof TaskDefinition)) {
+      return new TaskDefinition(name, opts);
+    }
+
+    // ensure we have opts
+    opts = opts || {};
+
+    // initialise the task name
+    this.name = name;
+
+    // initialise as not async
+    this.isAsync = false;
+
+    // initailise the dependencies to be an empty array
+    this._deps = _.uniq([].concat(opts.deps || []).concat(deps));
+
+    // allow a fallback task to be specified
+    this._fallback = opts.fallback || (defaults || {}).fallback;
   }
 
-  // ensure we have opts
-  opts = opts || {};
+  /**
+    ### depends(names)
+  **/
+  TaskDefinition.prototype.depends = function(names) {
+    var ownDep;
 
-  // initialise the task name
-  this.name = name;
+    // add some dependencies
+    this._deps = this._deps
+      .concat(names || [])
+      .concat(Array.prototype.slice.call(arguments, 1));
 
-  // initialise as not async
-  this.isAsync = false;
+    // remove any dependencies for this module name
+    while ((ownDep = this._deps.indexOf(this.name)) >= 0) {
+      this._deps.splice(ownDep, 1);
+    }
 
-  // initailise the dependencies to be an empty array
-  this._deps = _.uniq([].concat(opts.deps || []).concat(defaults.deps || []));
+    // chaining goodness
+    return this;
+  };
 
-  // allow a fallback task to be specified
-  this._fallback = opts.fallback || defaults.fallback;
-}
+  /**
+    ### isValid(missingDeps)
 
+    The valid method looks for the dependencies of the task and attempts to 
+    retrieve them from the taskify registry.  If all dependencies are resolved,
+    `isValid` will return true, or false if not.
 
-module.exports = TaskDefinition;
+    If the method is provided an array for the missingDeps argument, unresolved
+    task names will be pushed onto the array and can be accessed for diagnosis
+    of the error.
 
-/**
-  ### depends(names)
-**/
-TaskDefinition.prototype.depends = function(names) {
-  var ownDep;
+  **/
+  TaskDefinition.prototype.isValid = function() {
+    var missing = this.unresolved();
 
-  // add some dependencies
-  this._deps = this._deps
-    .concat(names || [])
-    .concat(Array.prototype.slice.call(arguments, 1));
+    // if we have no missing deps, check next level down
+    if (missing.length === 0) {
+      return this._deps.map(registry.get).filter(function(dep) {
+        return dep.isValid();
+      }).length === this._deps.length;
+    }
+  };
 
-  // remove any dependencies for this module name
-  while ((ownDep = this._deps.indexOf(this.name)) >= 0) {
-    this._deps.splice(ownDep, 1);
-  }
+  /**
+    ### unresolved()
 
-  // chaining goodness
-  return this;
-};
+    Return the names of any unresolved dependencies
+  **/
+  TaskDefinition.prototype.unresolved = function(deep) {
+    var missing = this._deps.filter(registry.missing);
 
-/**
-  ### isValid(missingDeps)
+    if (deep) {
+      this._deps.map(registry.get).filter(Boolean).forEach(function(dep) {
+        missing = missing.concat(dep.unresolved(true));
+      });
+    }
 
-  The valid method looks for the dependencies of the task and attempts to 
-  retrieve them from the taskify registry.  If all dependencies are resolved,
-  `isValid` will return true, or false if not.
+    return missing;
+  };
 
-  If the method is provided an array for the missingDeps argument, unresolved
-  task names will be pushed onto the array and can be accessed for diagnosis
-  of the error.
-
-**/
-TaskDefinition.prototype.isValid = function() {
-  var missing = this.unresolved();
-
-  // if we have no missing deps, check next level down
-  if (missing.length === 0) {
-    return this._deps.map(registry.get).filter(function(dep) {
-      return dep.isValid();
-    }).length === this._deps.length;
-  }
-};
-
-/**
-  ### unresolved()
-
-  Return the names of any unresolved dependencies
-**/
-TaskDefinition.prototype.unresolved = function(deep) {
-  var missing = this._deps.filter(registry.missing);
-
-  if (deep) {
-    this._deps.map(registry.get).filter(Boolean).forEach(function(dep) {
-      missing = missing.concat(dep.unresolved(true));
-    });
-  }
-
-  return missing;
+  return TaskDefinition;
 };
--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 /* jshint node: true */
 'use strict';
 
-// define the task registry
-var registry = require('./registry');
-var taskCounter = 1;
-var defaults = require('./defaults');
-var TaskDefinition = require('./definition');
-var ExecutionContext = require('./context');
-
 /**
   # Taskify
 
@@ -138,135 +131,152 @@ var ExecutionContext = require('./context');
 /**
   ## Taskify Reference
 
-  ### taskify(name, opts, runner)
 **/
-var taskify = module.exports = function(name, opts, runner) {
-  var task;
+module.exports = function(opts) {
+  // create the object hash for the registry, allow provision of an existing
+  // object that can be used in the registry in the case we want to unify
+  // tasks
+  var registry = (opts || {}).registry || {};
 
-  // handle the noopts case
-  if (typeof opts == 'function') {
-    runner = opts;
-    opts = {};
+  // create the task definition constructor
+  var TaskDefinition = require('./definition')(opts);
+
+  // define the ExecutionContext constructor
+  var ExecutionContext = require('./context')(registry, TaskDefinition, opts);
+
+  // initialise the task counter
+  var taskCounter = 1;
+
+  /**
+
+    ### task(name, opts, runner)
+
+    Register a new task with the taskify registry.
+
+  **/
+  function task(name, opts, runner) {
+    var task;
+
+    // handle the noopts case
+    if (typeof opts == 'function') {
+      runner = opts;
+      opts = {};
+    }
+    // if the opts is an array, we just have dependencies specified
+    else if (Array.isArray(opts)) {
+      opts = {
+        deps: opts
+      };
+    }
+
+    // create the task instance
+    // and save the new task instance to the registry
+    task = registry[name] = new TaskDefinition(name, opts);
+
+    // bind the exec function to the runner instance
+    task.runner = runner;
+
+    // return the task instance
+    return task;
   }
-  // if the opts is an array, we just have dependencies specified
-  else if (Array.isArray(opts)) {
-    opts = {
-      deps: opts
+
+  /**
+
+    ### task.get(name)
+
+    Retrieve a task from the registry.
+  **/
+  task.get = function(name) {
+    return registry[name];
+  }
+
+
+  /**
+    ### task.prepare
+
+    Prepare task(s) to execute, returning a function that will accept
+    arguments that will be passed through to the tasks
+  **/
+  task.prepare = function(target) {
+    var initArgs = Array.prototype.slice.call(arguments, 1);
+    var deps = [].concat(target || []);
+
+    // create a temporary task definition with deps on the specified target(s)
+    // TODO: generate a UUID for the task
+    var tmpTask = new TaskDefinition('ghost' + taskCounter, { deps: deps });
+
+    // increment the task counter
+    taskCounter += 1;
+
+    return function() {
+      var args = initArgs.concat(Array.prototype.slice.call(arguments));
+      var callback;
+      var proxy;
+
+      // if we have been supplied a function as the last argument
+      // then we will assume it is a callback function
+      // TODO: investigate making this toggleable with a taskify switch ?
+      if (typeof args[args.length - 1] == 'function') {
+        callback = args.pop();
+      }
+
+      // execute the task with the specified args
+      proxy = new ExecutionContext().exec(tmpTask, args);
+
+      // if we have a callback defined then attach it to the complete event 
+      // of the proxy
+      if (callback) {
+        proxy.once('complete', callback);
+      }
+
+      return proxy;
     };
-  }
-
-  // create the task instance
-  // and save the new task instance to the registry
-  task = registry.put(name, new TaskDefinition(name, opts));
-
-  // bind the exec function to the runner instance
-  task.runner = runner;
-
-  // return the task instance
-  return task;
-};
-
-/**
-  ### taskify.defaults
-
-  Update the defaults for taskify
-**/
-taskify.defaults = defaults;
-
-/**
-  ### taskify.get
-
-  Get a task by name
-**/
-taskify.get = registry.get;
-
-/**
-  ### taskify.prepare
-
-  Prepare task(s) to execute, returning a function that will accept arguments
-  that will be passed through to the tasks
-**/
-taskify.prepare = function(target) {
-  var initArgs = Array.prototype.slice.call(arguments, 1);
-  var deps = [].concat(target || []);
-
-  // create a temporary task definition with deps on the specified target(s)
-  // TODO: generate a UUID for the task
-  var tmpTask = new TaskDefinition('ghost' + taskCounter, { deps: deps });
-
-  // increment the task counter
-  taskCounter += 1;
-
-  return function() {
-    var args = initArgs.concat(Array.prototype.slice.call(arguments));
-    var callback;
-    var proxy;
-
-    // if we have been supplied a function as the last argument
-    // then we will assume it is a callback function
-    // TODO: investigate making this toggleable with a taskify switch ?
-    if (typeof args[args.length - 1] == 'function') {
-      callback = args.pop();
-    }
-
-    // execute the task with the specified args
-    proxy = new ExecutionContext().exec(tmpTask, args);
-
-    // if we have a callback defined then attach it to the complete event 
-    // of the proxy
-    if (callback) {
-      proxy.once('complete', callback);
-    }
-
-    return proxy;
   };
-};
 
-/**
-  ### taskify.select
+  /**
+    ### task.select
 
-  The select function passes control through to the `taskify.prepare`
-  function, but only once it has validated that task dependencies have been
-  satisfied.  If dependencies cannot be satisfied then an Error will
-  be thrown.
+    The select function passes control through to the `taskify.prepare`
+    function, but only once it has validated that task dependencies have been
+    satisfied.  If dependencies cannot be satisfied then an Error will
+    be thrown.
 
-**/
-taskify.select = function(target) {
-  var deps = [].concat(target || []);
-  var tmpDef = new TaskDefinition('tmp', { deps: deps });
-  var missingDeps = [];
+  **/
+  task.select = function(target) {
+    var deps = [].concat(target || []);
+    var tmpDef = new TaskDefinition('tmp', { deps: deps });
+    var missingDeps = [];
 
-  // if we have no dependencies then throw an exception
-  if (deps.length === 0) {
-    throw new Error('Task names are required to select tasks');
-  }
+    // if we have no dependencies then throw an exception
+    if (deps.length === 0) {
+      throw new Error('Task names are required to select tasks');
+    }
 
-  // if the temporary task is not valid, then also throw an error
-  if (! tmpDef.isValid(missingDeps)) {
-    var error = new Error('Unable to select task, unresolved dependencies: [' +
-         missingDeps.join(', ') + ']');
+    // if the temporary task is not valid, then also throw an error
+    if (! tmpDef.isValid(missingDeps)) {
+      var error = new Error('Unable to select task, unresolved dependencies: [' +
+           missingDeps.join(', ') + ']');
 
-    // add the missing dependencies array to the error
-    error.missing = [].concat(missingDeps);
-    throw error;
-  }
+      // add the missing dependencies array to the error
+      error.missing = [].concat(missingDeps);
+      throw error;
+    }
 
-  return taskify.prepare.apply(this, arguments);
-};
+    return taskify.prepare.apply(this, arguments);
+  };
 
-/**
-  ### taskify.reset
+  /**
+    ### task.run(target, args*)
 
-  Reset the registry - clear existing task definitions.
-**/
-taskify.reset = registry.reset;
+    Run the specified task, with the provided arguments.
 
-/**
-  ### taskify.run
-**/
-taskify.run = function(target) {
-  return taskify.prepare(target).apply(
-    null,
-    [].slice.call(arguments, 1)
-  );
+  **/
+  task.run = function(target) {
+    return task.prepare(target).apply(
+      null,
+      [].slice.call(arguments, 1)
+    );
+  };
+
+  return task;
 };
